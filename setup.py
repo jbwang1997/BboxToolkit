a6b75fdb1cea52f31be68cf06847c07f146c3705
jbwang1997 <709370615@qq.com>
2021-04-07 10:56:48 +0800

    change the path in split_configs

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='BboxToolkit',
       version='1.0',
-      description='a tiny toolkit for bounding boxes',
-      author='jbwang',
+      description='a tiny toolkit for special bounding boxes',
+      author='jbwang1997',
       packages=find_packages(),
       install_requires=[
           'opencv-python',
