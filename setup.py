0cfecc7e93925a5b1bd010ee4169aa219db5b4c6
jbwang1997 <jbwang1997@gmail.com>
2021-07-04 23:09:21 +0800

    More detail informations

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup, find_packages
 
-long_descriptions = '''
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
 
 
-setup(name='BboxToolkit',
-      version='2.0',
-      description='a tiny toolkit for spcial bounding boxes',
-      author='jbwang',
-      packages=find_packages(),
-      install_requires=[
-          'opencv-python',
-          'terminaltables',
-          'pillow',
-          'shapely',
-          'numpy'])
+if __name__ == '__main__':
+    setup(name='BboxToolkit',
+          version='2.0',
+          description='a tiny toolkit for spcial bounding boxes',
+          long_description=readme(),
+          long_description_content_type='text/markdown',
+          author='jbwang1997',
+          author_email='jbwang1997@gmail.com',
+          keywords='computer vision, object deteciton',
+          url=' https://github.com/jbwang1997/BboxToolkit',
+          packages=find_packages(),
+          license='Apache License 2.0',
+          python_requires='>=3.5',
+          install_requires=[
+              'pathos',
+              'opencv-python',
+              'terminaltables',
+              'shapely',
+              'numpy'])
